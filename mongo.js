907387337d148a77836aea3823eaeb2a2016f7a0
Leon Marbukh <leonmarbukh@gmail.com>
2020-12-29 16:26:17 -0800

    restructured proj

--- mongo.js
+++ mongo.js
@@ -1,36 +1,43 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-  content: 'HTML is Easy',
+  content: "HTML is Easy",
   date: new Date(),
   important: true,
-})
+});
 
 // note.save().then(result => {
 //   console.log('note saved!')
 //   mongoose.connection.close()
 // })
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
