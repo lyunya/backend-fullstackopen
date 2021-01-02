87883840ecd177286856d1523c8ee2bd8609af0b
Leon Marbukh <leonmarbukh@gmail.com>
2021-01-02 13:41:57 -0800

    uibuild

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
-app.use('./api/users', usersRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
